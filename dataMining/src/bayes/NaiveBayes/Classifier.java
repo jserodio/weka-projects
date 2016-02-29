fb73d477a9169306fcdb890d7f8f9adb4234eaba
jserodio <serodio.jose@protonmail.ch>
2016-03-01 00:29:43 +0100

    Commit desde Atom :-)

--- dataMining/src/bayes/NaiveBayes/Classifier.java
+++ dataMining/src/bayes/NaiveBayes/Classifier.java
@@ -5,43 +5,43 @@ import weka.classifiers.bayes.NaiveBayes;
 import weka.core.Instances;
 
 public class Classifier {
-	
+
 	NaiveBayes estimador;
-	
+
 	public Classifier() {
 		estimador= new NaiveBayes();//Naive Bayes
 	}
 
 
 	public void naiveBayes(Instances data) throws Exception{
-		 				
+
 				Evaluation evaluator;
-				
-					
-					evaluator = new Evaluation(data);	
-					
+
+
+					evaluator = new Evaluation(data);
+
 					int trainSize = (int) Math.round(data.numInstances() * 0.7);
 					int testSize = data.numInstances() - trainSize;
-					
+
 					Instances train = new Instances(data, 0, trainSize);
 					Instances test = new Instances(data, trainSize, testSize);
-					
+
 					// Aprender con el 70% de las instancias (train)
 					// creando el clasificador con el algoritmo Naive Bayes.
 					estimador.buildClassifier(train);
-					
+
 					// Dejar que prediga la clase estimada por el modelo para cada instancia del test
-					// y así después podremos comparar la clase real y la estimada
+					// y asÃ­ despuÃ©s podremos comparar la clase real y la estimada
 					evaluator.evaluateModel(estimador, test);
-					
+
 					double predictions[] = new double[test.numInstances()];
-					
+
 					// Recorrer todas las instancias
 					for (int i = 0; i < test.numInstances(); i++) {
 						predictions[i] = evaluator.evaluateModelOnceAndRecordPrediction(estimador, test.instance(i));
 						System.out.println(predictions[i]);
 					}
-					
+
 	}
-	
+
 }
